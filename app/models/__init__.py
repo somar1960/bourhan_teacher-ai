c299fb48d85e0987c51a0ba66bedbbd9b47bfa06
somar1960 <somarhh93@gmail.com>
2026-07-24 19:09:54 +0300

    Add additional model imports to __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,16 @@
 from app.database import Base
+
 from app.models.student import Student
 from app.models.group import Group
+from app.models.file import File
+
+from app.models.homework import Homework
+from app.models.homework_submission import HomeworkSubmission
+
+from app.models.exam import Exam
+from app.models.question import Question
+from app.models.option import Option
+from app.models.exam_result import ExamResult
+
+from app.models.ai_conversation import AIConversation
+from app.models.student_progress import StudentProgress
