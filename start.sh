ee7fd7f9139a7c6215a07adb756b09304f7e7f99
somar1960 <somarhh93@gmail.com>
2026-07-26 20:14:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "🔄 Running database migrations..."
-alembic upgrade head
+python -m alembic upgrade head
 
 echo "🚀 Starting Uvicorn server..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
