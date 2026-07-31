a40ca9bac49c5bd7601aa2342e2ec3ec3df62ac7
somar1960 <somarhh93@gmail.com>
2026-07-31 11:04:53 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,19 +3,17 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-
 engine = create_async_engine(
     settings.database_url,
     echo=False,
+    pool_pre_ping=True,
 )
 
-
 async_session = sessionmaker(
-    bind=engine,
+    engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
-
 
 class Base(DeclarativeBase):
     pass
